9f5dba60dd626410fd6b7fdd7c0e2cd44dc41476
Brent Nycum <brentnycum@gmail.com>
2014-03-26 21:00:42 -0500

    Initial commit.

---
+++ Example/BANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BANAppDelegate.h
+//  BANMLBScoreboard
+//
+//  Created by Brent Nycum on 3/26/14.
+//  Copyright (c) 2014 It's Brent. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BANAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Example/BANAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  BANAppDelegate.m
+//  BANMLBScoreboard
+//
+//  Created by Brent Nycum on 3/26/14.
+//  Copyright (c) 2014 It's Brent. All rights reserved.
+//
+
+#import "BANAppDelegate.h"
+
+@implementation BANAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
