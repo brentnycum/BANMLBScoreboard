56b594cf4a6a5bce39493a7650f693d8717211e5
Brent Nycum <brentnycum@gmail.com>
2014-03-26 21:24:49 -0500

    Game data.

--- BANMLBScoreboard/BANScoreboardGame.h
+++ BANMLBScoreboard/BANScoreboardGame.h
@@ -10,4 +10,38 @@
 
 @interface BANScoreboardGame : NSObject
 
+@property (nonatomic, retain) NSString *scoreboardId;
+
+@property (nonatomic, retain) NSString *homeTeamAbbrev;
+@property (nonatomic, retain) NSString *homeTeamCity;
+@property (nonatomic, retain) NSString *homeTeamName;
+@property (nonatomic, retain) NSNumber *homeTeamWins;
+@property (nonatomic, retain) NSNumber *homeTeamLoses;
+
+@property (nonatomic, retain) NSString *awayTeamAbbrev;
+@property (nonatomic, retain) NSString *awayTeamCity;
+@property (nonatomic, retain) NSString *awayTeamName;
+@property (nonatomic, retain) NSNumber *awayTeamWins;
+@property (nonatomic, retain) NSNumber *awayTeamLoses;
+
+@property (nonatomic, retain) NSString *gameStatus;
+@property (nonatomic, retain) NSString *gameStatusInd;
+@property (nonatomic, retain) NSNumber *gameInning;
+@property (nonatomic, assign) BOOL gameTopOfInning;
+
+@property (nonatomic, retain) NSNumber *awayTeamRuns;
+@property (nonatomic, retain) NSNumber *homeTeamRuns;
+
+@property (nonatomic, retain) NSNumber *awayTeamHits;
+@property (nonatomic, retain) NSNumber *homeTeamHits;
+
+@property (nonatomic, retain) NSNumber *awayTeamErrors;
+@property (nonatomic, retain) NSNumber *homeTeamErrors;
+
+@property (nonatomic, retain) NSString *homeTeamTV;
+@property (nonatomic, retain) NSString *homeTeamRadio;
+
+@property (nonatomic, retain) NSString *awayTeamTV;
+@property (nonatomic, retain) NSString *awayTeamRadio;
+
 @end
