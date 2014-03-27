5b06e097ef521e0ca087cccafe79a7da4af31f67
Brent Nycum <brentnycum@gmail.com>
2014-03-26 21:26:43 -0500

    Inning data.

---
+++ BANMLBScoreboard/BANScoreboardInning.h
@@ -0,0 +1,16 @@
+//
+//  BANScoreboardInning.h
+//  BANMLBScoreboard
+//
+//  Created by Brent Nycum on 3/26/14.
+//  Copyright (c) 2014 It's Brent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BANScoreboardInning : NSObject
+
+@property (nonatomic, retain) NSNumber *awayTeamRuns;
+@property (nonatomic, retain) NSNumber *homeTeamRuns;
+
+@end
