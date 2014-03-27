11b4226b1c3b25fd6c02bd7c3d5127d32167eb6b
Brent Nycum <brentnycum@gmail.com>
2014-03-26 21:09:28 -0500

    Starting BANScoreboardGame.

---
+++ BANMLBScoreboard/BANScoreboardGame.h
@@ -0,0 +1,13 @@
+//
+//  BANScoreboardGame.h
+//  BANMLBScoreboard
+//
+//  Created by Brent Nycum on 3/26/14.
+//  Copyright (c) 2014 It's Brent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BANScoreboardGame : NSObject
+
+@end
---
+++ BANMLBScoreboard/BANScoreboardGame.m
@@ -0,0 +1,13 @@
+//
+//  BANScoreboardGame.m
+//  BANMLBScoreboard
+//
+//  Created by Brent Nycum on 3/26/14.
+//  Copyright (c) 2014 It's Brent. All rights reserved.
+//
+
+#import "BANScoreboardGame.h"
+
+@implementation BANScoreboardGame
+
+@end
